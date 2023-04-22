cdff505ef72614a082e61bce9b303bae4f015947
hardkill551 <hardkill194@gmail.com>
2023-04-22 18:02:20 -0300

    Versão inicial

---
+++ src/components/SignInPage/SignIn.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export default function SignIn(){
+    return(
+        <Background>
+            <img></img>
+            <input/>
+            <input/>
+            <button/>
+            <button/>
+        </Background>
+    )
+}
+
+const Background = styled.div`
+background-color: #8C11BE;
+width:100vw;
+height:100vh;
+`
