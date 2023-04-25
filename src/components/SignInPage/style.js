eac87a17abfc4af93e74fced243e61d5ca5b4bbc
Hugo Costa <121950569+hardkill551@users.noreply.github.com>
2023-04-25 11:52:39 -0300

    Colocando dvh

--- src/components/SignInPage/style.js
+++ src/components/SignInPage/style.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 const Background = styled.div`
 background-color: #8C11BE;
 width:100vw;
-height:100vh;
+height:100dvh;
 display:flex;
 flex-direction:column;
 align-items:center;
